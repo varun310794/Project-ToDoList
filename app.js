3fbe53202a081cede9953524e384ffa4ec94e095
Varun Reddy Peddaiahgari <varun310794@gmail.com>
2018-11-12 23:24:12 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express=require("express");
+var app=express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("homepage.ejs");
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(req, res){
+    console.log("The Server has started");
+});
